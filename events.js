8ffa746439b4c35091b90f48e6d1ce159e730771
kevin1015carmona <kevin.git030301@gmail.com>
2022-11-24 23:31:22 -0600

    Empieza responsive header

---
+++ events.js
@@ -0,0 +1,8 @@
+"use strict";
+
+window.addEventListener("load", function () {
+  let button_menu = document.getElementById("button_menu");
+  button_menu.addEventListener("click", function () {
+    alert("hola");
+  });
+});
